1d012a005737b689087823d999f849754c464171
ryannguyen05 <baonguyenduongquoc@gmail.com>
2026-07-14 21:23:44 +0700

    Add Room model with schema definition

--- models/room.js
+++ models/room.js
@@ -1,1 +1,6 @@
-
+const mongoose = require("mongoose");
+const RoomSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    capacity: { type: Number, required: true }
+});
+module.exports = mongoose.model("Room", RoomSchema);
