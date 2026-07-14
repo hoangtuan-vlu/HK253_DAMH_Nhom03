6e9285e150d06d82baf21bdafa18086040407678
hoangtuan-vlu <tuanvo.04032006@gmail.com>
2026-07-14 18:52:37 +0700

    Room .js
    Remove Room model definition and export.

--- models/room.js
+++ models/room.js
@@ -1,8 +1,1 @@
-const mongoose = require("mongoose");
 
-const RoomSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    capacity: { type: Number, required: true }
-});
-
-module.exports = mongoose.model("Room", RoomSchema);
